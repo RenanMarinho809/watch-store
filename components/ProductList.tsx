b86009ef8bd121149d7474529d4ff915112d4a8d
RenanMarinho809 <renanmarinho809@gmail.com>
2025-09-15 20:38:46 -0300

    Doing again the watch store

---
+++ components/ProductList.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const products = ["", "", ""];
+
+const ProductList = () => {
+  return (
+    <div
+      id="product"
+      className="px-4 md:px-12 py-5 md:py-10 flex justify-center items-center"
+    >
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+        {products.map((product, index) => (
+          <Link href={`/product/123`}>
+            <Image
+              src="/hero-img.png"
+              alt="img"
+              width={1000}
+              height={1000}
+              className="max-w-[17rem] h-72 object-cover object-center rounded-lg"
+            />
+            <div className="mt-4">
+              <h2 className="font-semibold text-lg">Ivicta</h2>
+              <p className="font-medium text-sm mt-1">$2300</p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
