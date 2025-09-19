d2656be67e885e7e2ca31e491b430ef6afd4ac6e
RenanMarinho809 <renanmarinho809@gmail.com>
2025-09-19 15:02:48 -0300

    Added new fuctions

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,7 +3,8 @@
 import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import React, { Suspense, useEffect, useState } from "react";
 
 interface Product {
   _id: string;
@@ -12,14 +13,22 @@ interface Product {
   price: number;
 }
 
-const ProductList = () => {
+const SearchComponent = () => {
   const [products, setProducts] = useState([]);
+  const searchParams = useSearchParams();
 
   useEffect(() => {
-    axios
-      .get("/api/fetch-products")
-      .then((response) => setProducts(response.data.products));
-  }, []);
+    const searchTermFromUrl = searchParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      axios
+        .get(`/api/search?searchTerm=${searchTermFromUrl}`)
+        .then((response) => setProducts(response.data.products))
+        .catch((error) =>
+          console.log("Error fetching search results: ", error)
+        );
+    }
+  }, [searchParams]);
+
   return (
     <div
       id="product"
@@ -47,4 +56,12 @@ const ProductList = () => {
   );
 };
 
-export default ProductList;
+const SearchPage = () => {
+  return (
+    <Suspense fallback={"Hello"}>
+      <SearchComponent />
+    </Suspense>
+  );
+};
+
+export default SearchPage;
